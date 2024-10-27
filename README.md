07d73fc01bea2bca377de375e68ab4d13f23be97
GMkonan <guilhemont@hotmail.com>
2024-10-27 10:05:21 -0300

    Add more flags

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ ghe grabs gh and enhances the experience
 https://cli.github.com/manual/gh_repo_create
 
 ### ideas
-- change to rc or cr (repo create)
-- flags for public, private, push stuff...
 - list the options that you are gonna agree to and have the (Y/n)
+
+
+### Next commands to implement sane defaults
+- gh auth (awful amount of questions)
 
 For now way to run it is `go build` and then `go install`
--- cmd/cr.go
+++ cmd/cr.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"path/filepath"
 	"strings"
+	"sync"
 
 	"github.com/spf13/cobra"
 )
@@ -26,28 +27,41 @@ func ExecGhCmd(command string, args ...string) error {
 	if err != nil {
 		return fmt.Errorf("failed to create stdout pipe: %w", err)
 	}
+
 	if err := cmd.Start(); err != nil {
 		return fmt.Errorf("Command Failed to start: %v", err)
 	}
-	scanner := bufio.NewScanner(stderr)
-	for scanner.Scan() {
-		fmt.Println(scanner.Text())
-	}
 
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-		return fmt.Errorf("error reading stderr: %v", err)
-	}
+	var wg sync.WaitGroup
+	wg.Add(2)
 
-	scanner2 := bufio.NewScanner(stdout)
-	for scanner2.Scan() {
-		fmt.Println(scanner2.Text())
-	}
+	go func() {
+		defer wg.Done()
+		scannerForStderr := bufio.NewScanner(stderr)
+		for scannerForStderr.Scan() {
+			fmt.Println(scannerForStderr.Text())
+		}
 
-	if err := scanner2.Err(); err != nil {
-		log.Fatal(err)
-		return fmt.Errorf("error reading stdout: %v", err)
-	}
+		if err := scannerForStderr.Err(); err != nil {
+			log.Printf("error reading stderr: %v", err)
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+
+		scannerForStdout := bufio.NewScanner(stdout)
+		for scannerForStdout.Scan() {
+			fmt.Println(scannerForStdout.Text())
+		}
+
+		if err := scannerForStdout.Err(); err != nil {
+			log.Fatal(err)
+			log.Printf("error reading stdout: %v", err)
+		}
+	}()
+
+	wg.Wait()
 
 	if err := cmd.Wait(); err != nil {
 		log.Fatal(err)
@@ -57,11 +71,65 @@ func ExecGhCmd(command string, args ...string) error {
 	return nil
 }
 
+func confirmAction() bool {
+	reader := bufio.NewReader(os.Stdin)
+
+	for {
+		// fmt.Print("Do you want to proceed? [y/N]: ")
+		confirmationText := fmt.Sprintf(`Confirm the options:
+			Visibility: Public
+			Push local commits: Yes
+			Do you want to proceed? [y/N]
+		`)
+		fmt.Print(confirmationText)
+		response, err := reader.ReadString('\n')
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading input: %v\n", err)
+			return false
+		}
+
+		response = strings.ToLower(strings.TrimSpace(response))
+
+		switch response {
+		case "y", "yes":
+			return true
+		case "n", "no", "":
+			return false
+		default:
+			fmt.Println("Please answer 'y' or 'n'")
+		}
+	}
+}
+
+func getDirName() string {
+	currentDir, err := os.Getwd()
+
+	if err != nil {
+		fmt.Printf("Error grabing the path to dir")
+	}
+	dirName := filepath.Base(currentDir)
+
+	// make sure dirName is valid for github
+	if strings.Contains(dirName, " ") {
+		dirName = strings.ReplaceAll(dirName, " ", "-")
+	}
+	return dirName
+}
+
 func init() {
 	rootCmd.AddCommand(rpCmd)
 
+	var Org string
+	rpCmd.Flags().StringVar(&Org, "org", "", "Define a organization to create your repo into")
+
+	var IsPrivate bool
+	rpCmd.Flags().BoolVar(&IsPrivate, "private", false, "Create repo as private")
+
 	var Push bool
 	rpCmd.Flags().BoolVar(&Push, "push", false, "Push to repo")
+
+	var Confirm bool
+	rpCmd.Flags().BoolVar(&Confirm, "confirm", false, "Confirm create repo action without prompting")
 }
 
 var rpCmd = &cobra.Command{
@@ -74,29 +142,45 @@ var rpCmd = &cobra.Command{
 		- 
 	`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Start create repo command")
 
-		currentDir, err := os.Getwd()
-
-		if err != nil {
-			fmt.Printf("Error grabing the path to dir")
-		}
-		dirName := filepath.Base(currentDir)
-
-		// make sure dirName is valid for github
-		if strings.Contains(dirName, " ") {
-			dirName = strings.ReplaceAll(dirName, " ", "-")
-		}
 		// upstream?
-		ghBaseArgs := []string{"gh", "repo", "create", "--private", "--source=.", "--remote=origin"}
+		ghBaseArgs := []string{"gh", "repo", "create"}
 		ghFullArgs := make([]string, len(ghBaseArgs))
 
 		copy(ghFullArgs, ghBaseArgs)
+
+		org, _ := cmd.Flags().GetString("org")
+		if org != "" {
+			dirName := getDirName()
+			orgAndProject := org + "/" + dirName
+			ghFullArgs = append(ghFullArgs, orgAndProject)
+		}
+
+		// source
+		ghFullArgs = append(ghFullArgs, "--source=.")
+
+		// remote
+		ghFullArgs = append(ghFullArgs, "--remote=origin")
+
+		isPrivate, _ := cmd.Flags().GetBool("private")
+		if isPrivate == true {
+			ghFullArgs = append(ghFullArgs, "--private")
+		} else {
+			ghFullArgs = append(ghFullArgs, "--public")
+		}
+
 		push, _ := cmd.Flags().GetBool("push")
 		if push == true {
 			ghFullArgs = append(ghFullArgs, "--push")
 		}
-		fmt.Println(ghFullArgs)
+
+		confirm, _ := cmd.Flags().GetBool("confirm")
+		if !confirm && !confirmAction() {
+			fmt.Println("Operation cancelled")
+			return
+		}
+
+		// fmt.Println(ghFullArgs)
 		errCmd := ExecGhCmd(ghFullArgs[0], ghFullArgs[1:]...)
 
 		if errCmd != nil {
